ff050ca29f7575f60618990ecc2ff0a1e070e583
shaxzod shokirov <shokirovshaxzod32@gmail.com>
2022-09-06 21:11:26 +0500

    init: start project

---
+++ src/component/Header/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import './Header.scss';
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo-main">
+          <img src="https://htmlreference.io/images/html-reference-icon.png" alt="icon" />
+          <img src="https://htmlreference.io/images/html-reference-type.png" alt="icon" />
+        </div>
+        <div className="header_content">
+          <div className="header_ad_box">
+            <div className="header-carbon">
+              <span>
+                <span className="carbon-wrap">
+                  <a
+                    href="https://srv.carbonads.net/ads/click/x/GTND42JNCV7D523YC6YLYKQNCY7D6K3EF67DEZ3JCYSD627YFTSDK23KC6BIVKQIC67ICKQWF6ADC5QYCVYDPKJLHEYIK2JLCVSD52JECTNCYBZ52K?segment=placement:htmlreferenceio;"
+                    className="carbon-img"
+                    target="_blank"
+                    rel="noopener sponsored noreferrer">
+                    <img
+                      src="https://cdn4.buysellads.net/uu/1/122503/1661961403-SumoLogic-CarbonREVISED.png"
+                      alt="ads via Carbon"
+                      border="0"
+                      height="100"
+                      width="130"
+                    />
+                  </a>
+                  <a
+                    href="https://srv.carbonads.net/ads/click/x/GTND42JNCV7D523YC6YLYKQNCY7D6K3EF67DEZ3JCYSD627YFTSDK23KC6BIVKQIC67ICKQWF6ADC5QYCVYDPKJLHEYIK2JLCVSD52JECTNCYBZ52K?segment=placement:htmlreferenceio;"
+                    className="carbon-text"
+                    target="_blank"
+                    rel="noopener sponsored noreferrer">
+                    Sumo Logic named a Challenger in 2022 Gartner Magic Quadrant for APM and
+                    Observability
+                  </a>
+                </span>
+                <a
+                  href="http://carbonads.net/?utm_source=htmlreferenceio&amp;utm_medium=ad_via_link&amp;utm_campaign=in_unit&amp;utm_term=carbon"
+                  className="carbon-poweredby"
+                  target="_blank"
+                  rel="noopener sponsored noreferrer">
+                  ads via Carbon
+                </a>
+              </span>
+            </div>
+          </div>
+          <div className="header_text">
+            <h2 className="header-title">A free guide to HTML</h2>
+            <p className="header-subtitle">
+              <strong>Learn by example</strong>:
+              <a href="https://htmlreference.io">htmlreference.io</a> is a free guide to HTML. It
+              features all <strong>elements</strong> and <strong>attributes</strong>.
+            </p>
+            <footer className="header-share">
+              <a
+                className="share-button share-button--twitter"
+                href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fhtmlreference.io&amp;text=HTML+Reference%3A+a+free+guide+to+all+HTML+elements+and+attributes."
+                rel="external nofollow noreferrer"
+                target="_blank">
+                <svg
+                  className="svg svg--twitter"
+                  viewBox="0 0 300.00006 244.18703"
+                  height="24"
+                  width="30"
+                  version="1.1">
+                  <g transform="translate(-539.18 -568.86)">
+                    <path
+                      d="m633.9 812.04c112.46 0 173.96-93.168 173.96-173.96 0-2.6463-0.0539-5.2806-0.1726-7.903 11.938-8.6302 22.314-19.4 30.498-31.66-10.955 4.8694-22.744 8.1474-35.111 9.6255 12.623-7.5693 22.314-19.543 26.886-33.817-11.813 7.0031-24.895 12.093-38.824 14.841-11.157-11.884-27.041-19.317-44.629-19.317-33.764 0-61.144 27.381-61.144 61.132 0 4.7978 0.5364 9.4646 1.5854 13.941-50.815-2.5569-95.874-26.886-126.03-63.88-5.2508 9.0354-8.2785 19.531-8.2785 30.73 0 21.212 10.794 39.938 27.208 50.893-10.031-0.30992-19.454-3.0635-27.69-7.6468-0.009 0.25652-0.009 0.50661-0.009 0.78077 0 29.61 21.075 54.332 49.051 59.934-5.1376 1.4006-10.543 2.1516-16.122 2.1516-3.9336 0-7.766-0.38716-11.491-1.1026 7.7838 24.293 30.355 41.971 57.115 42.465-20.926 16.402-47.287 26.171-75.937 26.171-4.929 0-9.7983-0.28036-14.584-0.84634 27.059 17.344 59.189 27.464 93.722 27.464"
+                      fill="#1da1f2"></path>
+                  </g>
+                </svg>
+              </a>
+
+              <a
+                className="share-button share-button--facebook"
+                href="https://www.facebook.com/sharer.php?u=https%3A%2F%2Fhtmlreference.io"
+                rel="external nofollow noreferrer"
+                target="_blank">
+                <svg
+                  className="svg svg--facebook"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  width="26px"
+                  height="26px"
+                  viewBox="0 0 266.893 266.895"
+                  enableBackground="new 0 0 266.893 266.895">
+                  <path
+                    fill="#3C5A99"
+                    d="M248.082,262.307c7.854,0,14.223-6.369,14.223-14.225V18.812
+  c0-7.857-6.368-14.224-14.223-14.224H18.812c-7.857,0-14.224,6.367-14.224,14.224v229.27c0,7.855,6.366,14.225,14.224,14.225
+  H248.082z"></path>
+                  <path
+                    fill="#FFFFFF"
+                    d="M182.409,262.307v-99.803h33.499l5.016-38.895h-38.515V98.777c0-11.261,3.127-18.935,19.275-18.935
+  l20.596-0.009V45.045c-3.562-0.474-15.788-1.533-30.012-1.533c-29.695,0-50.025,18.126-50.025,51.413v28.684h-33.585v38.895h33.585
+  v99.803H182.409z"></path>
+                </svg>
+              </a>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
